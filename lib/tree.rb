41a2674296e91f1c0ed95533bd3910ec3494e5d4
teklumezgebo <mezge002@umn.edu>
2023-08-26 21:36:05 -0500

    commit tree 426d3c71d72d6757bbc27c5cad1a0efd2218b4a2

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,46 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  def breadth_first_traversal(node)
+    result = []
+    nodes_to_visit = [node]
+
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      result.push(node[:value])
+      nodes_to_visit = nodes_to_visit + node[:children]
+    end
+
+    result
+  end
+
+  def depth_first_traversal(node)
+    result = []
+    nodes_to_visit = [node]
+
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      result.push(node[:value])
+      nodes_to_visit = node[:children] + nodes_to_visit
+    end
+
+    result
+  end
+
+  def get_element_by_id(str)
+    nodes_to_visit = [@root]
+
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      if node[:id] == str
+        return node
+      else
+        nodes_to_visit = node[:children] + nodes_to_visit
+      end
+    end
+
+    nil
+  end
+
 end
